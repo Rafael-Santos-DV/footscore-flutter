6c1cc2138f02a79c84880a83dee3399ab9930db7
Rafael Santos <santosrafaell187@gmail.com>
2022-10-22 10:12:28 -0300

    feat: add master login screen background

--- lib/src/screens/login.dart
+++ lib/src/screens/login.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -12,18 +11,28 @@ class _Login extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+        padding: const EdgeInsets.fromLTRB(10, 30, 10, 0),
         color: const Color.fromRGBO(16, 19, 44, 1),
-        child: Column(
-          children: <Widget>[
-            Padding(
-                padding: const EdgeInsets.fromLTRB(0, 20, 20, 0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Image.asset("lib/src/assets/logo.png"),
-                    ])),
-          ],
-        ));
+        child: Column(children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.fromLTRB(0, 20, 20, 0),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Image.asset("lib/src/assets/logo.png"),
+                  ])),
+          Padding(
+              padding: const EdgeInsets.fromLTRB(0, 30, 20, 0),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset("lib/src/assets/halland-bg.png")
+                  ])),
+          const Text("Dicover All about sport",
+              style: TextStyle(
+                  decoration: TextDecoration.none,
+                  color: Colors.white,
+                  fontSize: 50))
+        ]));
   }
 }
